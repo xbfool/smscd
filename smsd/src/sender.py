ece0d880780ca582df0405c66ccd25506df36a65
xbfool <xbfool@gmail.com>
2011-06-28 01:41:57 +0800

    for sd_ct 182 bug

--- smsd/src/sender.py
+++ smsd/src/sender.py
@@ -581,9 +581,15 @@ class sms_sender(object):
                     count += 1
                 elif setting.get('sub_mode') == 'sd_ct':
                     print "in sd_ct.."
-                    self.__zhttp_pool.req(channel,  {'user_uid':user_uid, 'setting':setting, 'uid':uid, 'msg_num':msg_num, 'percent':percent},
-                                          address = ';'.join(address_list), content = msg, uid = '12345678901',
-                                        pwd = 'fd1234')
+                    address_f = [address_list[i] for i in range(len(address_list)) if address_list[i][0:3] != '182']
+                    if len(address_f) > 0:
+                        self.__zhttp_pool.req(channel,  {'user_uid':user_uid, 'setting':setting, 'uid':uid, 'msg_num':msg_num, 'percent':percent},
+                                              address = ';'.join(address_f), content = msg, uid = '12345678901',
+                                            pwd = 'fd1234')
+                    else:
+                        self.__zhttp_pool.req(channel,  {'user_uid':user_uid, 'setting':setting, 'uid':uid, 'msg_num':msg_num, 'percent':percent},
+                                              address = ';'.join(address_list), content = msg, uid = '12345678901',
+                                            pwd = 'fd1234')
                     count += 1
                     
                 elif setting.get('sub_mode') == 'honglian':
