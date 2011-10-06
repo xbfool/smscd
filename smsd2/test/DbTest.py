da780dae4d9a5a14b8f854ac51615662090d859b
xbfool <xbfool@gmail.com>
2011-10-06 15:39:36 +0800

    [smsd2] modify mysqldbtest bug

--- smsd2/test/DbTest.py
+++ smsd2/test/DbTest.py
@@ -6,13 +6,19 @@ Created on 2011-10-5
 import unittest
 from smsd2.config.config_reader import loadcfg
 from smsd2.database.db_engine import create_db
+from sqlalchemy import Column, Integer, Table, MetaData
 from traceback import print_exc
 class DbTest(unittest.TestCase):
     def testDb(self):
         try:
             cfg= loadcfg('mysqldb.yaml')
             db = create_db(cfg)
-            self.failIf(db == None)
+            meta = MetaData()
+            t = Table('testt', meta,
+            Column('uid', Integer, primary_key = True),
+            )
+            meta.create_all(bind = db, tables =[t])
+            self.failUnless(db)
         except:
             print_exc()
             self.fail('testdb Faile')
@@ -20,9 +26,14 @@ class DbTest(unittest.TestCase):
     def testDbFail(self):
         try:
             cfg= loadcfg('mysqldb.yaml')
-            cfg['database']['host'] = 'www.google.com'
+            cfg.database.password = '123'
             db = create_db(cfg)
-            self.failUnless(db == None)
+            meta = MetaData()
+            t = Table('testt', meta,
+            Column('uid', Integer, primary_key = True),
+            )
+            meta.create_all(bind = db, tables =[t])
+            self.failIf(db)
         except:
             pass
         
