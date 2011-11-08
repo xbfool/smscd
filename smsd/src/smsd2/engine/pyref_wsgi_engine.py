bf98df695028d843eb8c783828717b292507ae14
xbfool <xbfool@gmail.com>
2011-11-08 15:14:16 +0800

    remove smsd/smsd2

--- smsd/src/smsd2/engine/pyref_wsgi_engine.py
+++
@@ -1,8 +0,0 @@
-from wsgiref import simple_server
-
-class PyRefWsgiEngine(object):
-    def __init__(self, port, app):
-        self.server = simple_server.make_server('', 8000, app)
-    
-    def run(self):
-        self.server.serve_forever()
--- smsd/src/smsd2/template/wsgi.py
+++
@@ -1,6 +0,0 @@
-'''
-Created on 2011-10-4
-
-@author: xbfool
-'''
-
