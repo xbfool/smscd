2ecc58152d11f5a58f094d12d7c81de8820def80
xbfool <xbfool@gmail.com>
2012-01-30 17:14:03 +0800

    增加多余10个log删除机制

--- script/backup.smscd.sms.xbfool.org.sh
+++ script/backup.smscd.sms.xbfool.org.sh
@@ -1,6 +1,18 @@
 p=`date +%Y-%m-%d-%T`
-p1=~/home/xbfool/backup/smsd-$p.sql
-p2=~/home/xbfool/backup/smsd-$p.sql.tar.gz
+p1=/home/xbfool/backup/smsd-$p.sql
+p2=/home/xbfool/backup/smsd-$p.sql.tar.gz
 mysqldump -u root -pftp3*8*ing --opt -e smsd > $p1
 tar -cvzf $p2 $p1
 rm $p1
+
+p=`ls -t1 /home/xbfool/backup/*`
+index=10#0
+for i in $p
+do
+  let "index = index + 1"
+  if (($index > 10))
+  then
+    echo "deleting $i"
+    rm $i
+  fi
+done
