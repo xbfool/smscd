43e3c28e66aa34dd5a1bcf10ebeed777e331c791
xbfool <xbfool@gmail.com>
2013-11-05 16:00:02 +0800

    fix intent

--- smsd2/sender/settings.py
+++ smsd2/sender/settings.py
@@ -361,20 +361,20 @@ class sender_settings():
             'process_req' : process_req_zhangshangtong
         }
         #106eb
-				settings['zhangshangtong_03'] = {
-						'name': 'zhangshangtong_03',
-						'host': 'pi.f3.cn',
-						'path': '/SendSMS.aspx',
-						'port': '80',
-						'sub_mode': 'zhangshangtong',
-						'ececcid':'305990002',
-						'password':'jtyh888',
-						'msgtype':'5',
-						'longcode':'111',
-						'mode': 'POST',
-						'process_ret': process_ret_zhangshangtong,
-						'process_req' : process_req_zhangshangtong
-				}
+        settings['zhangshangtong_03'] = {
+            'name': 'zhangshangtong_03',
+            'host': 'pi.f3.cn',
+            'path': '/SendSMS.aspx',
+            'port': '80',
+            'sub_mode': 'zhangshangtong',
+            'ececcid':'305990002',
+            'password':'jtyh888',
+            'msgtype':'5',
+            'longcode':'111',
+            'mode': 'POST',
+            'process_ret': process_ret_zhangshangtong,
+            'process_req' : process_req_zhangshangtong
+        }
         settings['106f_95559'] = {
             'name': '106f_95559',
             'host': '221.179.180.158',
