8af2a97b01ccc0b2423e5fb67726876f51a1d7f8
xbfool <xbfool@gmail.com>
2014-04-09 11:54:53 +0800

    106k可以不需要签名

--- smsd/src/smsd_mod.py
+++ smsd/src/smsd_mod.py
@@ -430,6 +430,8 @@ class smsd(object):
         msg_status = message.F_FAIL
         if self.check_message_content(msg):
             msg_status = message.F_ADMIT
+        if channel == '106k':
+            msg_status = message.F_ADMIT
 
         if channel in ('changshang_a_01', 'changshang_a_02', 'changshang_a_03',
                         'honglian_01',
