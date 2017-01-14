de7c26eb81f1e086755c413b9acfa4d897ce257d
xbfool <xbfool@gmail.com>
2017-01-14 13:57:38 +0800

    fix bug

--- smsd2/sender/process_req.py
+++ smsd2/sender/process_req.py
@@ -619,7 +619,6 @@ def process_req_106i(http_pool, setting, msg):
         single_num = 0
     tmpmsg = safe_utf8_2_gbk(msg['content'])
     p = hashlib.md5(setting['password']).hexdigest()
-    m.update("Nobody inspects")
     http_pool.req(msg['channel'],
                   {'user_uid':msg['user_uid'],
                    'setting':setting,
